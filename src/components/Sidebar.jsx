bc8277aea6734e7853b3ffb37436f06dcec8874f
Harii-27 <hari534164@gmail.com>
2024-12-15 05:31:55 +0530

    Files added

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,16 @@
+import Search from "./Search";
+import Chats from "./chats";
+
+
+
+const Sidebar = () => {
+  return (
+    <div className="sidebar">
+      <Search />
+   
+      <Chats />
+    </div>
+  );
+};
+
+export default Sidebar;

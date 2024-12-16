e8834b2ec950cd57fc63b7c309482272a299a1b3
Harii-27 <hari534164@gmail.com>
2024-12-16 21:13:57 +0530

    changes

--- src/components/UserSelect/UserSelect.tsx
+++ src/components/UserSelect/UserSelect.tsx
@@ -2,48 +2,65 @@ import { useDispatch } from "react-redux";
 import { setCurrentUser } from "../../Store/chatSlice";
 import { User } from "../../types";
 import "./UserSelect.css";
-import React from "react";
+import React, { useState } from "react";
 
-const sampleUser: User[] = [
-  {
-    id: "1",
-    name: "User 1",
-    avatar:
-      "https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?w=100&h=100&fit=crop",
-    online: true,
-  },
-  {
-    id: "2",
-    name: "User 2",
-    avatar:
-      "https://images.unsplash.com/photo-1599566150163-29194dcaad36?w=100&h=100&fit=crop",
-    online: true,
-  },
- 
-];
+import defaultAvatar from '../../img/virat.png'; // Import the default avatar
+
+// Single dummy user
+const dummyUser: User = {
+  id: "1",
+  name: "Harii",
+  avatar: defaultAvatar,  
+  online: true,
+};
 
 export default function UserSelect() {
   const dispatch = useDispatch();
+  const [username, setUsername] = useState(dummyUser.name); // Pre-fill with the dummy user name
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-  const handleSelectUser = (user: User) => {
-    dispatch(setCurrentUser(user));
+  const handleLogin = () => {
+    if (username.trim()) {
+      // If the username is provided, set the current user
+      dispatch(setCurrentUser({ ...dummyUser, name: username }));
+      setIsLoggedIn(true);
+    }
   };
 
   return (
     <div className="user-select-wrapper">
-      <h1 className="user-select-heading">Select a user</h1>
-      <div className="user-select-list">
-        {sampleUser.map((user) => (
-          <div
-            key={user.id}
-            onClick={() => handleSelectUser(user)}
-            className="user-select-item"
-          >
-            <img src={user.avatar} alt={user.name} className="user-avatar" />
-            <p className="user-name">{user.name}</p>
+      {!isLoggedIn ? (
+        <div className="login-container">
+          <h1 className="login-heading">Login</h1>
+
+          {/* Display the user's avatar on the login screen */}
+          <img src={dummyUser.avatar} alt={dummyUser.name} className="login-avatar" />
+
+          <input
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)} // Handle changes in the input
+            placeholder="Enter your username"
+            className="username-input"
+          />
+          <button onClick={handleLogin} className="login-button">
+            Login
+          </button>
+        </div>
+      ) : (
+        <div className="user-select-wrapper">
+          <h1 className="user-select-heading">Welcome, {username}</h1>
+          <div className="user-select-list">
+            <div
+              onClick={() => dispatch(setCurrentUser(dummyUser))}
+              className="user-select-item"
+            >
+              <img src={dummyUser.avatar} alt={dummyUser.name} className="user-avatar" />
+              <p className="user-name">{dummyUser.name}</p>
+            </div>
           </div>
-        ))}
-      </div>
+        </div>
+      )}
     </div>
   );
 }
